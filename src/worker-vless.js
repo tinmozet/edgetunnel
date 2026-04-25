ce515993cf79a3d554201efe2e2c22a9a4e9eb72
tinmozet <151650203+tinmozet@users.noreply.github.com>
2026-04-25 19:23:13 +0630

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,9 +4,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '38a96526-22c2-4c12-a46f-5ebfb6dbcac4';
 
-let proxyIP = '';
+
+
+let proxyIP = 'cdn.anycast.eu.org';
 
 
 if (!isValidUUID(userID)) {
